08555cdc6885150c8e066daca368c6cbf60907b4
Samuethus <matheusv5703@gmail.com>
2023-12-03 01:38:14 -0300

    AJUSTES

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const NavMenu = document.querySelector("#navbar-items");
 menu.addEventListener("click" , () => {
     menu.classList.toggle("ativo");
     NavMenu.classList.toggle("ativo");
-   
-
-
 })
+
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
